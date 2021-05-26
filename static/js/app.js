a3b40720a6ab069db8e8b51de2069637dfd2d6b8
LBerez <lberez@mac.com>
2021-05-26 14:55:06 -0500

    adding screenshots

--- static/js/app.js
+++ static/js/app.js
@@ -55,12 +55,6 @@ function updateFilters() {
   
     // 8. Set the filtered data to the tableData.
     let filteredData = tableData;
-
-    console.log(filters);
-    for (key in filters) {
-
-      console.log(`${key}: ${filters[key]}`);
-  }
   
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
